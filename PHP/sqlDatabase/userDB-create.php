926ca3ba587185a775028465c08fde2d114f72ac
Joshua Lambley <48166293+JoshuaLambley@users.noreply.github.com>
2021-01-12 16:31:28 -0500

    Update userDB-create.php

--- PHP/sqlDatabase/userDB-create.php
+++ PHP/sqlDatabase/userDB-create.php
@@ -15,9 +15,4 @@ catch (Exception $e)
 }
 
 $hash = password_hash('password', PASSWORD_DEFAULT); 
-
-$db->insert('test1@test.com', 'Test1', 'Test1', $hash, 'admin');
-$db->insert('test2@test.com', 'Test2', 'Test2', $hash, 'user');
-$db->insert('test3@test.com', 'Test3', 'Test3', $hash, 'user');
-
 ?>
